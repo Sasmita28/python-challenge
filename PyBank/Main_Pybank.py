9f7661605f2e2122c660534d8ac1dae5ac3ff6a9
sasmitapraveen <sasmitapraveen@gmail.com>
2020-02-05 15:04:27 -0600

    python file with output

--- PyBank/Main_Pybank.py
+++ PyBank/Main_Pybank.py
@@ -4,8 +4,12 @@ import os
 # Module for reading CSV files
 import csv
 
+
+# giving path for the input as well as output files
 csvpath = os.path.join('Resources', 'budget_data.csv')
+
 output_path = os.path.join("output", "PyBank.txt")
+
 # Reading using CSV module
 
 with open(csvpath, newline='') as csvfile:
@@ -14,19 +18,21 @@ with open(csvpath, newline='') as csvfile:
 
    csv_header = next(csvreader)
 
+   # intializing the variables
    num_month = 0
    net_total = 0
-
    profit_col = []
  
-   
+   # Calculating no. of months and net total by looping through csvreader
    for row in csvreader:
       
       num_month += 1
       net_total = net_total + int(row[1])
+
+      #appending profit/loss column values to profit_col
       profit_col.append(row[1])
       
-# casting the profit/losses column as integers
+      # casting the profit/losses column as integers
       profit_col = [int(i) for i in profit_col]
 
 
@@ -38,21 +44,25 @@ with open(csvpath, newline='') as csvfile:
          date_min = row[0]
 
    change_total = 0
-#finding the change 
+   #finding the change 
    for i in range(len(profit_col) - 1):
 
-      
       change = profit_col[i + 1] - profit_col[i]
 
+      # Finding the max chage  and min change using max  and min profit
       if  profit_col[i + 1] == max(profit_col):
          change_max = change
       elif profit_col[i + 1] == min(profit_col):
          change_min = change
-   
+
+
+      # calculating the total chage
       change_total = change_total + change
- 
+      #calculating the average change
       avg_change = change_total / (len(profit_col) - 1)
 
+
+   #Printing the results to the terminal
    print(f"Total Months: {num_month}")
    print(f"Total: ${net_total}")
    print(f"Average Change: ${round(avg_change,2)}")
@@ -62,9 +72,9 @@ with open(csvpath, newline='') as csvfile:
  
 
 # Open the file using "write" mode. Specify the variable to hold the contents
-with open(output_path, 'w', newline='') as writer:
+with open(output_path, 'w') as writer:
 
-    # Writing the lines to the text file 
+   # Writing the lines to the PyBank.text file 
     writer.writelines(f"Total Months: {num_month}\n")
 
     writer.writelines(f"Total: ${net_total}\n")
